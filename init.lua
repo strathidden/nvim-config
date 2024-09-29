8d33a363f174787a64cf72f43f7d5563d852ab77
rainxbb <zrainxbb@gmail.com>
2024-09-29 16:12:08 +0200

    removed auto format on save

--- init.lua
+++ init.lua
@@ -567,23 +567,12 @@ require("lazy").setup({
         },
         opts = {
             notify_on_error = false,
-            format_on_save = function(bufnr)
-                local disable_filetypes = { c = true, cpp = true }
-                local lsp_format_opt
-                if disable_filetypes[vim.bo[bufnr].filetype] then
-                    lsp_format_opt = 'never'
-                else
-                    lsp_format_opt = 'fallback'
-                end
-                return {
-                    timeout_ms = 500,
-                    lsp_format = lsp_format_opt,
-                }
-            end,
             formatters_by_ft = {
                 lua = { 'stylua' },
                 tsx = { 'prettierd', 'prettier' },
                 ts = { 'prettierd', 'prettier' },
+                go = { 'gopls' },
+                templ = { 'templ' },
             },
         },
     },
